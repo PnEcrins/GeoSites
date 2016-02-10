3260f8171aceef37e27ed114af50cad8bdb5e0ff
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-02-10 17:06:35 +0100

    gestion des intérets par requete ajax sans préchargement

--- js/controllers/modalDetailsController.js
+++ js/controllers/modalDetailsController.js
@@ -7,13 +7,22 @@ app.controller('ModalDetailsCtrl', function ($scope, $http, $uibModalInstance, s
 	$scope.afficherSuite = function (chaine) {
 		$scope.longueur = chaine.length + 1;
 	}
-	var url = 'generatedocs.php?id_site=' + $scope.site.id_site;
-	$http.get(url).success(function(response) {
+    
+    //chargement des documents du site
+	var urlDocs = 'generatedocs.php?id_site=' + $scope.site.id_site;
+	$http.get(urlDocs).success(function(response) {
 		// Tableau d'objets contenant les informations à propos des fichiers pdf pour un seul site passé en paramètre
 		$scope.docs = response;
 	});
 
-
+    //chargement des intérêts du site
+	var urlInterets = 'generateinterets.php?id_site=' + $scope.site.id_site;
+	$http.get(urlInterets).success(function(response) {
+		// Tableau d'objets contenant les informations à propos des fichiers pdf pour un seul site passé en paramètre
+		$scope.interets = response;
+	});
+    
+    //fermeture de la modale
 	$scope.ok = function () {
 		$uibModalInstance.close();
 	};
