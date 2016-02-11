89e4536c324eda188891bd9acc6c1e8f9bbc4264
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-02-11 08:31:54 +0100

    bricoles

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,6 +1,6 @@
 // Création du controller HomeController
 // Un controller gère les données de l'application
-app.controller('HomeController', ['$scope', 'htmlcontent', '$uibModal', '$http','LeafletServices', '$location', '$anchorScroll', '$rootScope', function ($scope, htmlcontent, $uibModal, $http, LeafletServices, $location, $anchorScroll, $rootScope) {
+app.controller('HomeController', ['$scope', 'htmlcontent', '$uibModal', '$http','LeafletServices', 'loadingService', '$location', '$anchorScroll', '$rootScope', function ($scope, htmlcontent, $uibModal, $http, LeafletServices, loadingService, $location, $anchorScroll, $rootScope) {
 
 	// Initialisation
 	$scope.loadingClass = 'onloading';
@@ -38,14 +38,17 @@ app.controller('HomeController', ['$scope', 'htmlcontent', '$uibModal', '$http',
 	$scope.openAccueil();
 
 	$scope.openDetails = function (site) {
+        loadingService.showPleaseWait();
 		var modalInstance = $uibModal.open({
 			templateUrl: 'templates/modalDetails.html',
 			controller: 'ModalDetailsCtrl',
 			size: 'lg',
 			resolve: {
 				site: function () {
+                    loadingService.hidePleaseWait();
 					return site;
 				}
+                
 			}
 		});
 	};	
@@ -129,8 +132,7 @@ app.controller('HomeController', ['$scope', 'htmlcontent', '$uibModal', '$http',
 				};
 			}
 		);
-		$scope.loadingClass = 'isload';
-		openAccueil();		
+		$scope.loadingClass = 'isload';		
 	})
 	.error(function(err) {
 		$scope.erreur = err;
@@ -218,5 +220,17 @@ app.factory('LeafletServices', ['$http', function($http) {
 		return this.layer;
 	  }	  
    };
+}]);
+
+app.factory('loadingService', [function() {
+        var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+        return {
+            showPleaseWait: function() {
+                pleaseWaitDiv.modal();
+            },
+            hidePleaseWait: function () {
+                pleaseWaitDiv.modal('hide');
+            },
+        };
 }]);
 
