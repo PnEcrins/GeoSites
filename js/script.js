06d5a508334841faa00a7253044a6f1d32ff9ba1
romthi38 <thibault.romanin@gmail.com>
2016-02-11 09:06:09 +0100

    Suppression par erreur

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Gestion événementielle du bouton afficher/cacher de la sidebar
+$(document).ready(function(){
+    $("show-sidebar").click(function(){
+        $("#sidebar").toggle();
+        if($("#sidebar").is(":visible")){
+        	var isVisible = true;
+            document.getElementById("containerCarte").className = "col-md-offset-4 col-md-8";
+            carte.invalidateSize();            
+        }
+        else{
+        	var isVisible = false;
+            document.getElementById("containerCarte").className = "col-md-12";
+            carte.invalidateSize();
+        }
+    });
+});
