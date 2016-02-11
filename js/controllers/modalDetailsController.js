6239e0c90acef62e3126f597deef394d38717790
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-02-11 08:30:55 +0100

    mise en place de l'intégration des pdf dans la modale - cf #32

--- js/controllers/modalDetailsController.js
+++ js/controllers/modalDetailsController.js
@@ -1,6 +1,33 @@
 // Controller de la modal, ici on s'occupe de ce qui est fermeture au clic sur le bouton
 app.controller('ModalDetailsCtrl', function ($scope, $http, $uibModalInstance, site) {
-	$scope.site = site;
+	
+
+  $scope.pdfName = 'my test so ggod';
+  $scope.pdfUrl = 'data/pdf/PAC-0002-Dessin.pdf';
+  $scope.scroll = 0;
+  $scope.loading = 'loading';
+
+  $scope.getNavStyle = function(scroll) {
+    if(scroll > 100) return 'pdf-controls fixed';
+    else return 'pdf-controls';
+  }
+
+  $scope.onError = function(error) {
+    console.log(error);
+  }
+
+  $scope.onLoad = function() {
+    $scope.loading = '';
+  }
+
+  $scope.onProgress = function(progress) {
+    console.log(progress);
+  }
+    
+    // Initialisation
+    $scope.site = site;
+
+	// On récupère dans le fichier Json toutes les données de la barre de navigation (le logo, le titre, les liens)
 	$scope.longueurChaineAfficherSuite = longueurChaineAfficherSuite;
 	$scope.longueur = $scope.longueurChaineAfficherSuite;
 
