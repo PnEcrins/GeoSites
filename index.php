0e926ed47ce34827eb3a6e4fa193f167ac60bd0c
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-01-30 11:37:51 +0100

    Ajout du suivi Google Analytics. Fix #65

--- index.php
+++ index.php
@@ -57,6 +57,18 @@ include ("dbconnect.php");
 		<script src="js/controllers/modalAideController.js"></script>
 		<script src="js/controllers/modalContactController.js"></script>
 		<script src="js/controllers/modalPneController.js"></script>
+		
+		<!-- Google Analytics -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+		  ga('create', '<?php echo $ID_GA;?>', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+		
 	</body>
 </html>
