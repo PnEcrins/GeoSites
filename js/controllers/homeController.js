e53dba0e350180a3b40b1c2f8cd2e58e80f57d8a
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-01-31 16:12:05 +0100

    Ordonnr les interets du filtre

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -203,16 +203,16 @@ app.controller('HomeController', ['$scope','$rootScope', '$timeout', '$compile',
                         "name" : "Intérêt géologique"
                         ,"values" : {
                             "Géomorphologie":{"label":"Géomorphologie", "visible":true}
+							,"Hydrogéologie":{"label":"Hydrogéologie", "visible":true}
+							,"Hydrothermalisme":{"label":"Hydrothermalisme", "visible":true}
                             ,"Métamorphisme":{"label":"Métamorphisme", "visible":true}
+							,"Minéralogie":{"label":"Minéralogie", "visible":true}
+							,"Paléontologie":{"label":"Paléontologie", "visible":true}
                             ,"Plutonisme":{"label":"Plutonisme", "visible":true}
                             ,"Ressources naturelles":{"label":"Ressources naturelles", "visible":true}
+							,"Stratigraphie":{"label":"Stratigraphie", "visible":true}
                             ,"Tectonique":{"label":"Tectonique", "visible":true}
-                            ,"Hydrothermalisme":{"label":"Hydrothermalisme", "visible":true}
-                            ,"Hydrogéologie":{"label":"Hydrogéologie", "visible":true}
-                            ,"Stratigraphie":{"label":"Stratigraphie", "visible":true}
-                            ,"Minéralogie":{"label":"Minéralogie", "visible":true}
                             ,"Volcanisme":{"label":"Volcanisme", "visible":true}
-                            ,"Paléontologie":{"label":"Paléontologie", "visible":true}
                         }
                     }
                 };
